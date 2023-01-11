39216fc4fbbfbe0c8ac8e0715c4e18db0b06708a
Adrianl93 <105607840+Adrianl93@users.noreply.github.com>
2023-01-11 17:59:59 -0300

    Update book_controller.js

--- src/controller/book_controller.js
+++ src/controller/book_controller.js
@@ -77,9 +77,9 @@ async function createDbBooks() {
         (item) =>
           item.volumeInfo.hasOwnProperty("description") &&
           item.volumeInfo.hasOwnProperty("imageLinks") &&
-          item.volumeInfo.authors.length &&
+          item.volumeInfo.authors?.length &&
           item.volumeInfo.categories &&
-          item.volumeInfo.categories.length &&
+          item.volumeInfo.categories?.length &&
           item.volumeInfo.pageCount &&
           item.volumeInfo.publishedDate &&
           item.volumeInfo.averageRating
